98e4d3fbc1a66bc7d9b6b416a68b915f9be150fc
Darwin Algarin <dalgarins@live.com>
2023-10-16 17:54:59 -0500

    Added documentation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,6 +61,8 @@ tasks {
     patchPluginXml {
         sinceBuild.set("222")
         untilBuild.set("233.*")
+        changeNotes.set(file("src/main/html/change-notes.html").inputStream().readBytes().toString(Charsets.UTF_8))
+        pluginDescription.set(file("src/main/html/description.html").inputStream().readBytes().toString(Charsets.UTF_8))
     }
 
     publishPlugin {
