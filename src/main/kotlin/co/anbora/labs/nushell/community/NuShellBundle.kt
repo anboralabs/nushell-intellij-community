245e4215ef308dedd23887f3bb5104c3a382143c
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2026-03-02 22:43:34 -0500

    add configuration provider.

---
+++ src/main/kotlin/co/anbora/labs/nushell/community/NuShellBundle.kt
@@ -0,0 +1,16 @@
+package co.anbora.labs.nushell.community
+
+import com.intellij.DynamicBundle
+import org.jetbrains.annotations.NonNls
+import org.jetbrains.annotations.PropertyKey
+
+@NonNls
+private const val BUNDLE = "messages.NuShellBundle"
+
+object NuShellBundle : DynamicBundle(BUNDLE) {
+
+    fun message(
+        @PropertyKey(resourceBundle = BUNDLE) key: String,
+        vararg params: Any
+    ): String = getMessage(key, *params)
+}
