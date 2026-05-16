9a14b7bd36710e70a501a30fc060c0c835cbdbe2
Darwin Esteban Algarin Sarmiento <dalgarins@live.com>
2026-05-16 12:20:00 -0500

    upgrade sdk.

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,8 +7,8 @@ fun environment(key: String) = providers.environmentVariable(key)
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "2.3.20"
-    id("org.jetbrains.intellij.platform") version "2.15.0"
-    id("org.jetbrains.intellij.platform.grammarkit") version "2.15.0"
+    id("org.jetbrains.intellij.platform") version "2.16.0"
+    id("org.jetbrains.intellij.platform.grammarkit") version "2.16.0"
 }
 
 group = properties("pluginGroup").get()
@@ -16,7 +16,7 @@ version = properties("pluginVersion").get()
 
 // Set the JVM language level used to build the project.
 kotlin {
-    jvmToolchain(21)
+    jvmToolchain(25)
 }
 
 // Configure project's dependencies
